8beb495285d326f5175480881fd4940b6929dbee
moussaelaq <moussa.el39@gmail.com>
2025-09-23 10:16:35 +0100

    initial commit

---
+++ src/test/kotlin/com/respiroc/shopifyreaisync/ShopifyReaiSyncApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.respiroc.shopifyreaisync
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShopifyReaiSyncApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
